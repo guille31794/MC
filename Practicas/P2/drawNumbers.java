78dc87672645d67aae35e87eb94f087a9671c372
Guillermo Girón García <guille.g94@hotmail.es>
2020-02-20 21:19:00 +0100

    Started drawing class

---
+++ Practicas/P2/drawNumbers.java
@@ -0,0 +1,18 @@
+/*
+ * @author Guillermo Girón García 
+ * Software que permite pintar pares de 
+ * coordenadas en un gráfico
+ */
+
+ public class drawNumbers extends Canvas
+ {
+     public drawNumbers()
+     {
+
+     }
+
+     public void paint(Graphic g)
+     {
+
+     }
+ }
