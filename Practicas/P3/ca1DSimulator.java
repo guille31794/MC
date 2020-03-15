f3e05d3cb3f6f86d81ef365f0ec5b54633160e68
Guillermo Girón García <guille.g94@hotmail.es>
2020-03-15 12:56:07 +0100

    CA completed. Need to be tested

--- Practicas/P3/ca1DSimulator.java
+++ Practicas/P3/ca1DSimulator.java
@@ -1,41 +1,81 @@
-import java.util.ArrayList;
-
 /*
 * @author Guillermo Girón García
 * Clase que implementa el automata 
 * celular 1D
 */
 
-// Reglas
-// k ^ k^ (2 * r + 1)
-// Tamaño vector
-// K ^ 2 * r + 1
+import java.util.ArrayList;
 
 public class ca1DSimulator implements ca1DSim
 {
     private int[] ca, rulesTable;
-    private int k, r, additiveRule;
+    private int k, r;
     // Condicion cilindrica -> True
     // Condicion nula -> False
-    private String binaryNumber;
     private boolean boundCondition;
-
+    
     public ca1DSimulator(ArrayList<Integer> ar,
     int k_, int r_, boolean bc, int adr)
     {
         k = k_;
         r = r_;
         ca = ar.toArray();
-        rulesTable = new int[(int)Math.pow(k, (2 * r) + 1)];    
-        binaryNumber = new String();
+        rulesTable = new int[(int)Math.pow(k, (2 * r) + 1)];
         boundCondition = bc;
         additiveRule = adr;
+
+        for(int i = rulesTable.length - 1; adr >= 0; adr/=k, --i)
+            rulesTable[i] = adr % k;
     }
 
     @Override
     public void nextGen()
     {
+        String differentBase;
+        int index, pow;
 
+        for (int i = 0; i < ca.length; ++i) 
+        {
+            differentBase = new String();
+            index = 0;
+
+            for (int j = i - r; j < i + r; ++j)
+            {
+                if (boundCondition)
+                    // Condicion de frontera cilindrica
+                    differentBase += nextGenCilindrical(j);
+                else
+                    // Condicion de frontera nula
+                    differentBase += nextGenNull(j);
+            }
+
+            pow = differentBase.length();
+
+            for (int j = 0; j < differentBase.length(); ++j, --pow)
+                index += Character.getNumericValue(differentBase.charAt(j)) * Math.pow(k, index);
+
+            ca[i] = rulesTable[index];
+        }
+    }
+
+    private char nextGenCilindrical(int j)
+    {
+        if (j < 0)
+             return ca[ca.length - 1 - j];
+        else if (j >= ca.length)
+            return ca[j % k];
+        else
+            return ca[j];
+    }
+
+    private char nextGenNull(int j)
+    {            
+        if (j < 0)
+            return "0";
+        else if (j >= ca.length)
+            return "0";
+        else
+            return ca[j];
     }
 
     @Override
