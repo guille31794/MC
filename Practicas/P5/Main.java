a8625cbefb90d80bf9d1df84946605779064c51e
Guillermo Girón García <guille.g94@hotmail.es>
2020-03-27 00:24:26 +0100

    Coded annalisis software and tested

--- Practicas/P5/Main.java
+++ Practicas/P5/Main.java
@@ -4,22 +4,25 @@
 * de simulación de las prácticas de la asignatura
 */
 
+import java.time.temporal.Temporal;
 import java.util.concurrent.*;
 
-import sun.jvm.hotspot.gc.shared.Space;
+//import sun.jvm.hotspot.gc.shared.Space;
 
 public class Main
 {
-    public static void main(String[] args) 
+    public static void main(String[] args) throws Exception
     {
         int nThreads = Runtime.getRuntime().availableProcessors(),
         start, end, frame, maxDistanceRule = -1, maxSpaEntropieRule = -1,
         maxTempEntropieRule = -1;
         double maxDistance = 0.0, maxSpaEntropie = 0.0,
         maxTempEntropie = 0.0;
+        TemporalEntropie ce;
+        randomGenerator r;
 
         // Randu
-        randomGenerator r = new randomGenerator(1000l, 7, 45); 
+        r = new randomGenerator(1000l, 7, 45);
 
         frame = 1000 / nThreads;
 
@@ -31,7 +34,7 @@ public class Main
 
             // Cellular automaton 
             // Bound Condition <- Null
-            ca1DSimulator.setCA(rg.getGenerated(), 2, 1, false, rule, 4000, nThreads);
+            ca1DSimulator.setCA(r.getGenerated(), 2, 1, false, rule, 4000, nThreads);
             
             for(int i = 0; i < nThreads; ++i)
             {
@@ -52,7 +55,7 @@ public class Main
             start = 1;
             end = frame;
 
-            for (int i = 0; i < threadNumber; ++i) 
+            for (int i = 0; i < nThreads; ++i) 
             {
                 tpe.execute(new hammingDistance(start, end));
                 start = end;
@@ -68,15 +71,14 @@ public class Main
             double meanDistance = hammingDistance.meanDistance();
 
             // Spacial entropie
-            SpacialEntropie.setParameters(ca.status(), k_);
+            SpacialEntropie.setParameters(ca1DSimulator.status(), 2);
             tpe = (ThreadPoolExecutor) Executors.newFixedThreadPool(nThreads);
             start = 0;
             end = frame;
 
-            for (int i = 0; i < threadNumber; ++i) 
+            for (int i = 0; i < nThreads; ++i) 
             {
-                se = new SpacialEntropie(start, end);
-                tpe.execute(se);
+                tpe.execute(new SpacialEntropie(start, end));
                 start = end + 1;
                 end += frame;
             }
@@ -91,11 +93,11 @@ public class Main
 
             // Temporal entropie
             double meanTempEntropie = 0.0;
-            TemporalEntropie.setParameters(ca.status());
+            TemporalEntropie.setParameters(ca1DSimulator.status());
             for(int i = 0; i < 1000; ++i)
             {
                 ce = new TemporalEntropie(i);
-                meanTempEntropie += ce;
+                meanTempEntropie += ce.Entropie();
             }
 
             meanTempEntropie /= 1000.0;
@@ -124,6 +126,6 @@ public class Main
         System.out.println("Max. Spa. Entropie: " + maxSpaEntropie +
         " Rule: " + maxSpaEntropieRule);
         System.out.println("Max. Temp. Entropie: " + maxTempEntropie +
-        " Rule: " + maxTempEntropie);
+        " Rule: " + maxTempEntropieRule);
     }
 }
